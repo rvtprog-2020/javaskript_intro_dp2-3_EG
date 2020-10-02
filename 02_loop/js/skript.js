43950abb2520bfa288144d5f90954a5ed20dd46e
Eduards Karlis Gudevics <93DPEKGudevics@rvt.lv>
2020-10-02 09:49:16 +0300

    awdh

---
+++ 02_loop/js/skript.js
@@ -0,0 +1,13 @@
+console.log("Loop task")
+
+function printHallo(){
+    console.log("Prive,eh ble")
+}
+
+function showpattern(totalRows){
+    for(let cerentRow = 1; curentRow <= totalRows; cerentRow++){
+
+    }
+}
+
+printHallo()
---
+++ 03_array/js/skript.js
@@ -0,0 +1,14 @@
+console.log("Sveiciens no konsoles")
+
+let mainigais1 = "viens"
+let mainigais2 = "divi"
+
+console.log('mainigais1:', mainigais1)
+console.log('mainigais2:', mainigais2)
+
+let temp = mainigais1
+mainigais1 = mainigais2
+mainigais2 = temp
+
+console.log('mainigais1:', mainigais1)
+console.log('mainigais2:', mainigais2)
